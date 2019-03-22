b9a77895c50c376328e929b3ae60f42a187518a0
PenPenMark06 <yuwa.yokohama.works@gmail.com>
2019-03-21 20:54:17 -0400

    Initialized a website

---
+++ my_portfolio/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'top' => "home#top"
+  get 'home/top' => "home#top"
+  get 'about' => "home#about"
+  get '/' => "home#top"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

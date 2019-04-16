682dcbbec3f4346e919f3ccc2a12df6997a07349
PenPenMark06 <yuwa.yokohama.works@gmail.com>
2019-04-15 22:49:45 -0400

    Commiting before chaning sqlite to pg

--- my_portfolio/config/routes.rb
+++ my_portfolio/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   get 'top' => "home#top"
   get 'home/top' => "home#top"
   get 'about' => "home#about"
   get '/' => "home#top"
+  get 'posts' => "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
